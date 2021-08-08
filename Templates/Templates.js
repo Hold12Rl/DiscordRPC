7e50ae45994ab763e58e46fded9dc0f03799cd90
Hold12Rl <86273091+Hold12Rl@users.noreply.github.com>
2021-08-07 20:15:01 -0700

    Create Templates.js

---
+++ Templates/Templates.js
@@ -0,0 +1,6 @@
+//So You want YOUR DiscordRPC for people To use
+//And Change them For THEIR code
+//Well i Got u
+//Use The template File Right in the Folder Template
+//THEN the person who looks In here Can Create A Pull Request
+//And ill Check it and YEA
